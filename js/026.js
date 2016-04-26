28c45eff3ca11a43be4a8c36bdd83d179f6fcac5
jake09 <nfreedom9@gmail.com>
2016-04-27 08:18:59 +0900

    1

--- js/026.js
+++ js/026.js
@@ -1,3 +1,6 @@
+var fn = require("./fn"),
+	_ = fn._;
+
 /*
  26.
  A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
@@ -16,8 +19,48 @@
  Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
  */
 
-function solve() {
+function unitFraction(denominator) {
+	function trans(no) {
+		if (!no) return 0;
+		for (;;) {
+			if (no >= denominator) return no;
+			no *= 10;
+		}
+	}
+	var temp = trans(1);
+	var info = [];
+	for (;;) {
+		var add = parseInt(temp / denominator);
+		if (!add || _.contains(info, add)) {
+			return (function(info, add) {
+				if (!add) return "0." + info.join("");
+				return _.reduce(info, function(str, n) {
+					if (n === add) return str + "(" + n;
+					return str + n;
+				}, "0.") + ")";
+			})(info, add);
+		}
+		info.push(add);
+		temp = trans(temp % denominator);
+	}
+}
 
+console.log(_.map(_.range(2,100), function(n) { return ['1/'+n, unitFraction(n), 1/n] }));
+
+//console.log(unitFraction(3).indexOf(')') - unitFraction(3).indexOf('('));
+//console.log(unitFraction(4).indexOf(')') - unitFraction(4).indexOf('('));
+//console.log(unitFraction(5).indexOf(')') - unitFraction(5).indexOf('('));
+//console.log(unitFraction(6).indexOf(')') - unitFraction(6).indexOf('('));
+//console.log(unitFraction(7).indexOf(')') - unitFraction(7).indexOf('('));
+
+function solve() {
+	return _.reduce(_.range(3,1000), function(result, n) {
+		//console.log(result);
+		var fStr = unitFraction(n);
+		var fStr0 = unitFraction(result);
+		if (fStr.indexOf(')') - fStr.indexOf('(') > fStr0.indexOf(')') - fStr0.indexOf('(')) return n;
+		return result;
+	}, 2);
 }
 
 console.log("# # # # # # # # # # # # # # # # # # # # 026 # # # # # # # # # # # # # # # # # # # #");
