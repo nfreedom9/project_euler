586a415bccb21a176b78df547571f75c3c7d0d3e
freedom9 <nfreedom9@gmail.com>
2016-05-03 09:21:33 +0900

    .

--- bin/test.js
+++ bin/test.js
@@ -1,4 +1,9 @@
-require("../js/026");
-require("../js/027");
+for (var i = 1; i < 10; i++) {
+    require("../js/00" + i);
+}
+
+for (var j = 10; j < 33; j++) {
+    require("../js/0" + j);
+}
 
 console.log("# # # # # # # # # # # # # # # # # # # # END # # # # # # # # # # # # # # # # # # # #");
